debaf2b6ae252b6c34e57b89d89c6547982561e6
Stefan Siegl <stefan.siegl@novatec-gmbh.de>
2015-04-01 20:16:28 +0200

    removed version tag, remove unnecessary license

--- build-image.sh
+++ build-image.sh
@@ -14,12 +14,7 @@ if [ ! -e $osgipackages ] ; then
 	errors=true
 fi
 
-if [ ! -e license.lic ] ; then
-	echo "FATAL: Please provide a license.lic file. The EM starts without a license, so if you do not need any, just remove this line and remove the adding of the license in the dockerfile."
-	errors=true
-fi
-
 if [ "$errors" = false ] ; then
 	echo "Starting the build"
-	docker build -t stefansiegl/introscope-em:9.6.0.0 .
+	docker build -t stefansiegl/introscope-em .
 fi
